c8ba0eb0606136de51ea3d39f795f2b4c1560838
Francesco Komauli <francesco.komauli@gmail.com>
2016-05-27 13:06:51 +0200

    Animation of shutter closing on Qu

--- Assets/ThreeBladesLevel.cs
+++ Assets/ThreeBladesLevel.cs
@@ -8,6 +8,9 @@ public class ThreeBladesLevel : MonoBehaviour {
 
     const float SIZE = 6f;
     const float MAX_OPENING = 0.25f;
+    const float MIN_OPENING = 0.04f;
+
+    bool finalClosing = false;
 
     void Awake() {
         shutter.bladesNumber = 3;
@@ -16,10 +19,24 @@ public class ThreeBladesLevel : MonoBehaviour {
     }
 
     void Update() {
-        if (shutter.opening == 0) {
+        if (finalClosing) { return; }
+        if (shutter.opening == 0f) {
             shutter.opening = MAX_OPENING;
+        } else if (shutter.opening <= MIN_OPENING) {
+            finalClosing = true;
+            StartCoroutine(FinalClosingAnimation());
         } else {
             shutter.opening -= closingSpeed * Time.deltaTime;
         }
+    }
+
+    IEnumerator FinalClosingAnimation() {
+        yield return new WaitForSeconds(0.5f);
+        while (shutter.opening >= 0.0001f) {
+            shutter.opening /= 1.1f;
+            yield return null;
+        }
+        shutter.opening = 0f;
+        finalClosing = false;
     }
 }
