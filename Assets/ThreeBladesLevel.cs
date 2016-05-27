3e47841574489ea6e6d857a4ff227f1c549381e9
Francesco Komauli <francesco.komauli@gmail.com>
2016-05-27 12:29:05 +0200

    Blades closing animation

---
+++ Assets/ThreeBladesLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThreeBladesLevel : MonoBehaviour {
+
+    public Shutter shutter;
+    public float closingSpeed;
+
+    const float SIZE = 6f;
+    const float MAX_OPENING = 0.25f;
+
+    void Awake() {
+        shutter.bladesNumber = 3;
+        shutter.relativeSize = SIZE;
+        shutter.opening = MAX_OPENING;
+    }
+
+    void Update() {
+        if (shutter.opening == 0) {
+            shutter.opening = MAX_OPENING;
+        } else {
+            shutter.opening -= closingSpeed * Time.deltaTime;
+        }
+    }
+}
