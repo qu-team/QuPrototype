2883c09c7b4d6eaff7799de54e747a50a491ddd7
Francesco Komauli <francesco.komauli@gmail.com>
2016-05-27 12:57:52 +0200

    Added Qu at the center of the shutter

---
+++ Assets/Qu/Qu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Qu : MonoBehaviour {
+
+    public GameObject eyes;
+
+    SpriteRenderer sprite;
+
+    void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+        StartCoroutine(MoveEyes());
+    }
+
+    public void SetColor(Color color) {
+        sprite.color = color;
+    }
+
+    IEnumerator MoveEyes() {
+        while (isActiveAndEnabled) {
+            if (Random.value < 0.02f) {
+                var r = Random.value * 5f;
+                var a = Random.value * Mathf.PI * 2f;
+                eyes.transform.localPosition = new Vector2(Mathf.Cos(a), Mathf.Sin(a)) * r;
+            }
+            yield return null;
+        }
+    }
+}
