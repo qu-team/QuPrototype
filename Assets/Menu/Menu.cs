e68f0416038e5de788d95f91f91676fbeef09a49
Francesco Komauli <francesco.komauli@gmail.com>
2016-06-01 11:11:39 +0200

    Colorized other menu components

--- Assets/Menu/Menu.cs
+++ Assets/Menu/Menu.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
     public Qu qu;
+    public Button button;
+    public Text title;
 
     void Start() {
-        qu.Color = new Color(Random.value, Random.value, Random.value);
+        ColorizeMenuElements(new Color(Random.value, Random.value, Random.value));
+    }
+
+    void ColorizeMenuElements(Color color) {
+        qu.Color = color;
+        button.GetComponent<Image>().color = color;
+        var meanGrayColor = (color.r + color.g + color.b) / 3f;
+        button.GetComponentInChildren<Text>().color = (meanGrayColor < 0.5f) ? Color.white : Color.black;
+        title.color = color;
     }
 
     public void StartGame() {
-        SceneManager.LoadScene("Level");
+        button.interactable = false;
+        SceneManager.LoadSceneAsync("Level");
     }
 }
