16f1bcab41813c9b70deae3a0c4dbd844d75d3db
Francesco Komauli <francesco.komauli@gmail.com>
2016-07-18 10:14:03 +0200

    Score depends on time

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -1,13 +1,20 @@
-﻿public struct Score {
+﻿using UnityEngine;
+
+public struct Score {
 
     public uint basePoints;
     public float difficultyMultiplier;
 
     uint currentDifficulty;
+    float snapshot;
+
+    void Start() {
+        snapshot = Time.fixedTime;
+    }
 
     public uint Value {
         get {
-            return (uint)(basePoints + difficultyMultiplier * currentDifficulty * currentDifficulty);
+            return (uint)((basePoints + difficultyMultiplier * currentDifficulty * currentDifficulty) / (Time.fixedTime - snapshot));
         }
     }
 
@@ -15,9 +22,11 @@
 
     public void Succeeded() {
         currentDifficulty++;
+        snapshot = Time.fixedTime;
     }
 
     public void Failed() {
         currentDifficulty /= 2;
+        snapshot = Time.fixedTime;
     }
 }
