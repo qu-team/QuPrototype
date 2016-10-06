c8ae30ebb2113e5a1f15335c3836fa2d85c125c7
silverweed <silverweed1991@gmail.com>
2016-10-06 15:20:39 +0200

    get rid of the android plugin

--- Assets/Plugins/Android/jar.sh
+++
@@ -1,2 +0,0 @@
-# We use JDK 1.6 for compatibility 
-javac -source 1.6 -target 1.6 qu/GetLocale.java && rm -rf getlocale.jar && jar cf getlocale.jar qu/GetLocale.class
--- Assets/Plugins/Android/qu/GetLocale.java
+++
@@ -1,9 +0,0 @@
-package qu;
-
-import java.util.Locale;
-
-public final class GetLocale {
-	public static String getLocaleString() {
-		return Locale.getDefault().toString();
-	}
-}
