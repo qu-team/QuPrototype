9e301cb3add2cc8222cbd8f985804c3004e2afa0
silverweed <silverweed1991@gmail.com>
2017-01-17 16:43:13 +0100

    add back button to map

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -128,6 +128,7 @@ public class GameManager : MonoBehaviour {
         //Todo
         int lastlvl=0;
         mapManager.MoveCameraAtLevel(lastlvl);
+        currentState = GameState.MAP;
     }
 
 #endregion
--- Assets/Map/MapManager.cs
+++ Assets/Map/MapManager.cs
@@ -2,73 +2,77 @@ using UnityEngine;
 using Gestures;
 
 public class MapManager : MonoBehaviour{
-	public GesturesDispatcher dispatcher;
-	public LevelPopup popup;
-	public Vector2 mapDimension;
-	Vector3 orig;
-	bool justClosed;
+    public GesturesDispatcher dispatcher;
+    public LevelPopup popup;
+    public Vector2 mapDimension;
+    Vector3 orig;
+    bool justClosed;
 
 
-	void Start(){
-		SpriteRenderer sp = GetComponent<SpriteRenderer>();
-		mapDimension = new Vector2(sp.bounds.extents.x ,sp.bounds.extents.y);
-		GameManager.Instance.MapFinishedLoading(this);
-		dispatcher.OnTapEnd+= TapEnd;
-		dispatcher.OnSwipeProgress += SwipeProgress;
-		dispatcher.OnSwipeStart += SwipeStart;	
-	}
+    void Start(){
+        SpriteRenderer sp = GetComponent<SpriteRenderer>();
+        mapDimension = new Vector2(sp.bounds.extents.x ,sp.bounds.extents.y);
+        GameManager.Instance.MapFinishedLoading(this);
+        dispatcher.OnTapEnd+= TapEnd;
+        dispatcher.OnSwipeProgress += SwipeProgress;
+        dispatcher.OnSwipeStart += SwipeStart;    
+    }
 
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.Q))
-			Application.Quit();
-	}
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Q))
+            Application.Quit();
+    }
 
-	public void ClickedLevel(int level){
-		popup.ShowPopup(GameManager.Instance, level);	
-	}
+    public void ClickedLevel(int level){
+        popup.ShowPopup(GameManager.Instance, level);    
+    }
 
-	void TapEnd(Tap tap){
-		if(justClosed){
-			justClosed = false;
-			return;
-		}
-		if(popup.gameObject.activeSelf ) return;
-		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(tap.Position), Vector2.zero);
-		if(hit.collider==null) return;
-		LevelSelectButtons lv = hit.collider.gameObject.GetComponent<LevelSelectButtons>();
-		if (lv != null){
-			ClickedLevel(lv.level);
-		}
-	}
+    void TapEnd(Tap tap){
+        if(justClosed){
+            justClosed = false;
+            return;
+        }
+        if(popup.gameObject.activeSelf ) return;
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(tap.Position), Vector2.zero);
+        if(hit.collider==null) return;
+        LevelSelectButtons lv = hit.collider.gameObject.GetComponent<LevelSelectButtons>();
+        if (lv != null){
+            ClickedLevel(lv.level);
+        }
+    }
 
-	void SwipeProgress(Swipe swipe) {	
-		if(popup.gameObject.activeSelf ) return;
-		Camera camera = Camera.main;
-		Vector2 shift = (camera.ScreenToWorldPoint(swipe.End) -
-				camera.ScreenToWorldPoint(swipe.Start));
-		MoveCamera(shift);		
-	}
-	void SwipeStart(Swipe swipe) {
-		if(popup.gameObject.activeSelf) return;
-		orig =Camera.main.transform.position;
-	}
-	public void ClosePopup(){
-		justClosed= true;
-		popup.gameObject.SetActive(false);
-	}
+    void SwipeProgress(Swipe swipe) {    
+        if(popup.gameObject.activeSelf ) return;
+        Camera camera = Camera.main;
+        Vector2 shift = (camera.ScreenToWorldPoint(swipe.End) -
+                camera.ScreenToWorldPoint(swipe.Start));
+        MoveCamera(shift);        
+    }
+    void SwipeStart(Swipe swipe) {
+        if(popup.gameObject.activeSelf) return;
+        orig =Camera.main.transform.position;
+    }
+    public void ClosePopup(){
+        justClosed= true;
+        popup.gameObject.SetActive(false);
+    }
 
-	void MoveCamera(Vector2 shift){
-		Camera camera = Camera.main;
-		float xbound = mapDimension.x - camera.orthographicSize * camera.aspect,
-		      ybound = mapDimension.y - camera.orthographicSize;
-		camera.transform.position = new Vector3(
-				Mathf.Clamp(orig.x - shift.x, -xbound, xbound),
-				Mathf.Clamp(orig.y - shift.y, -ybound, ybound), orig.z);
+    void MoveCamera(Vector2 shift){
+        Camera camera = Camera.main;
+        float xbound = mapDimension.x - camera.orthographicSize * camera.aspect,
+              ybound = mapDimension.y - camera.orthographicSize;
+        camera.transform.position = new Vector3(
+                Mathf.Clamp(orig.x - shift.x, -xbound, xbound),
+                Mathf.Clamp(orig.y - shift.y, -ybound, ybound), orig.z);
 
-	}
+    }
 
-	public void MoveCameraAtLevel(int lvl){
-		Vector2 target = GameObject.Find("Level" + (lvl + 1)).transform.position;
-		MoveCamera(-target + (Vector2)Camera.main.transform.position);
-	}
+    public void MoveCameraAtLevel(int lvl){
+        Vector2 target = GameObject.Find("Level" + (lvl + 1)).transform.position;
+        MoveCamera(-target + (Vector2)Camera.main.transform.position);
+    }
+
+    public void BackButton() {
+        GameManager.Instance.Back();
+    }
 }
