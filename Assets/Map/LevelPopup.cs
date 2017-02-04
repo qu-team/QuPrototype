b27fcb1b998fee6cefd227a42a382ced962ff6db
silverweed <silverweed1991@gmail.com>
2017-02-04 11:35:59 +0100

    show lock panel on locked levels in Map

--- Assets/Map/LevelPopup.cs
+++ Assets/Map/LevelPopup.cs
@@ -2,37 +2,39 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class LevelPopup : MonoBehaviour{
-	public Text Info;
-	public Text Title;
+    public Text Info;
+    public Text Title;
 
     public GameObject playButton;
+    public GameObject lockedPanel;
+    public GameObject statsPanel;
     public Image star1;
     public Image star2;
     public Image star3;
 
     int level;
-	GameManager gm;
+    GameManager gm;
 
     void Awake() {
 
     }
 
-	public void ShowPopup(GameManager gm, int level, Color color){
-		if (level >= gm.Levels.Count) {
-			LogHelper.Warn(this, "Called ShowPopup(lv=" + (level + 1) + "), but only "
-					+ gm.Levels.Count + " levels have been loaded.");
-			return;
-		}
-		this.gm = gm;
-		print("level = " + (level + 1));
-		Title.text = (level + 1) + " - " + gm.Levels[level].name;	
-		LevelSaveData? lvdata = null;
-		if (GameData.data.levels != null && GameData.data.levels.Count > level)
-			lvdata = GameData.data.levels[level];
-		Info.text = L10N.Translate(L10N.Label.BEST_SCORE) + ":\n"+ (lvdata.HasValue ? lvdata.Value.maxScore.ToString() : "-")
-			+ "\n" + L10N.Translate(L10N.Label.SAVED_QUS) + ":\n" + (lvdata.HasValue ? lvdata.Value.quSaved.ToString() : "-")
-			+ "\n" + L10N.Translate(L10N.Label.TO_NEXT_LEVEL) + ":\n" + gm.Levels[level].quToNextLevel.ToString();
-		this.level = level;
+    public void ShowPopup(GameManager gm, int level, Color color){
+        if (level >= gm.Levels.Count) {
+            LogHelper.Warn(this, "Called ShowPopup(lv=" + (level + 1) + "), but only "
+                    + gm.Levels.Count + " levels have been loaded.");
+            return;
+        }
+        this.gm = gm;
+        print("level = " + (level + 1));
+        Title.text = (level + 1) + " - " + gm.Levels[level].name;    
+        LevelSaveData? lvdata = null;
+        if (GameData.data.levels != null && GameData.data.levels.Count > level)
+            lvdata = GameData.data.levels[level];
+        Info.text = L10N.Translate(L10N.Label.BEST_SCORE) + ":\n"+ (lvdata.HasValue ? lvdata.Value.maxScore.ToString() : "-")
+            + "\n" + L10N.Translate(L10N.Label.SAVED_QUS) + ":\n" + (lvdata.HasValue ? lvdata.Value.quSaved.ToString() : "-")
+            + "\n" + L10N.Translate(L10N.Label.TO_NEXT_LEVEL) + ":\n" + gm.Levels[level].quToNextLevel.ToString();
+        this.level = level;
         gameObject.SetActive(true);
         GetComponent<Image>().color = color;
         DisablePlayButtonIfLevelLocked();
@@ -42,6 +44,8 @@ public class LevelPopup : MonoBehaviour{
     void DisablePlayButtonIfLevelLocked() {
         var unlocked = level <= GameData.data.curLevelUnlocked;
         playButton.SetActive(unlocked);
+        lockedPanel.SetActive(!unlocked);
+        statsPanel.SetActive(unlocked);
     }
 
     void DisplayStars() {
@@ -58,7 +62,7 @@ public class LevelPopup : MonoBehaviour{
         }
     }
 
-	public void PlayLevel() {
-		gm.PlayLevel(level);
-	}
+    public void PlayLevel() {
+        gm.PlayLevel(level);
+    }
 }
