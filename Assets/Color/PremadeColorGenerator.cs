bbda5d231b03c2103c84027411a70d289edc252e
silverweed <silverweed1991@gmail.com>
2017-03-25 18:53:36 +0100

    work on premadecolorgenerator

--- Assets/Color/PremadeColorGenerator.cs
+++ Assets/Color/PremadeColorGenerator.cs
@@ -4,28 +4,66 @@ using System;
 using System.Linq;
 
 // Returns colors from a premade pool
+// FIXME: make difficulty useful
 public class PremadeColorGenerator : IColorGenerator {
 
     const string COLORS_FILE = "colors";
 
-    ColorTuples colorPool;
-    int idx = 0;
+    IList<ColorTuple> colorGroups;
+    IList<int> groupIdx;
+    int usedGroup;
 
-    public float Difficulty { get; set; }
+    public float Difficulty {
+        set {
+            usedGroup = colorGroups == null ? 0 : (int)Mathf.Min(value / 4, colorGroups.Count - 1);
+        }
+    }
 
     public void Initialize() {
-        colorPool = JsonUtility.FromJson<ColorTuples>(Resources.Load<TextAsset>(COLORS_FILE).text);
-        //colorPool.tuples.RemoveAll(tuple => tuple.level != GameManager.Instance.CurrentLevel);
-        //colorPool.tuples.Shuffle(new System.Random());
+        var colorPool = JsonUtility.FromJson<ColorTuples>(Resources.Load<TextAsset>(COLORS_FILE).text);
         Debug.Assert(colorPool != null && colorPool.tuples != null && colorPool.tuples.Count > 0);
         LogHelper.Ok(this, "Loaded " + colorPool.tuples.Count + " color tuples from Resources/" + COLORS_FILE);
+        colorPool.tuples.RemoveAll(tuple => tuple.level != GameManager.Instance.CurrentLevel);
+        //colorPool.tuples.Shuffle(new System.Random());
+        colorPool.tuples.OrderByDescending(tuple => tuple.de);
+        ShuffleBySameDE(colorPool);
     }
 
     public Color[] Generate(int n) {
-        var tuple = colorPool.tuples[idx];
-        Debug.Log(tuple.colors);
-        idx = (idx + 1) % colorPool.tuples.Count;
+        var tuple = colorGroups[usedGroup].tuples[groupIdx[usedGroup]];
+        groupIdx[usedGroup] = (groupIdx[usedGroup] + 1) % colorGroups[usedGroup].Count;
         return tuple.colors.Select(dc => new Color(dc.r/255f, dc.g/255f, dc.b/255f)).ToArray();
+    }
+
+    void ShuffleBySameDE(ColorTuples colorPool) {
+        colorGroups = DivideByDE(colorPool);
+        groupIdx = new List<int>(colorGroups.Count);
+        var rand = new System.Random();
+        foreach (var group in colorGroups) {
+            group.Shuffle(rand);
+            groupIdx.Add(0);
+        }
+    }
+
+    IList<IList<ColorTuple>> DivideByDE(ColorTuples colorPool) {
+        var list = new List<IList<ColorTuple>>();
+        int idx = 0;
+        float de = 0f;
+        List<ColorTuple> partial = null;
+        do {
+            var tup = colorPool.tuples[idx];
+            float nde = tup.de;
+            if (nde != de) {
+                if (partial != null)
+                    list.Add(partial);
+                partial = new List<ColorTuple>();
+            }
+            partial.Add(tup);
+            de = nde;
+            ++idx;
+        } while (idx < colorPool.tuples.Count);
+
+        return list;
     }
 }
 
